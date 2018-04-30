435088aaff720d803aa974a44f82b7dc7b8102c9
Sean Tucker <R00Ki3@users.noreply.github.com>
2018-04-29 20:58:35 -0500

    initial commit

---
+++ globalscripts/game_rules.lua
@@ -0,0 +1,10 @@
+--game_rules.lua 1.1 (5-30-2015) R00Kie
+
+if GAMEMODE == "CTF" then
+	-- For CTF game mode put the include script here
+
+
+	
+end
+
+-- For all game modes put the include script down here
